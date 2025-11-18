555dd706feec5daa6603f3fd55217a526b1b101b
Mateus <mateuzor@gmail.com>
2025-11-18 15:00:00 -0300

    add useNotifications hook

--- poc-web-notifications-api/src/App.tsx
+++ poc-web-notifications-api/src/App.tsx
@@ -4,11 +4,8 @@ export default function App() {
       <header>
         <div>
           <h1>poc-web-notifications-api</h1>
-          <div className="sub">
-            Web Notifications API PoC (bootstrap)
-          </div>
+          <div className="sub">Web Notifications API Poc</div>
         </div>
-        <span className="badge">React + Vite + TypeScript</span>
       </header>
     </div>
   );
