e11fbb8119a7d95db218e1b56078b4839b50956a
Mateus <mateuzor@gmail.com>
2025-12-11 15:20:00 -0300

    adds basic router

---
+++ poc-react-router-dom/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 16 }}>
+      <h1>Home Page</h1>
+    </div>
+  );
+}
---
+++ poc-react-router-dom/src/pages/NotFound.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div style={{ padding: 16 }}>
+      <h1>404</h1>
+      <p>Rota não encontrada.</p>
+    </div>
+  );
+}
