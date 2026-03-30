384b9bcc27d454ce2ba4dc802c2dd720e7450557
Mateus <mateuzor@gmail.com>
2026-03-30 09:12:34 -0300

    feat: add recoil dependency and RecoilRoot setup

--- poc-zustand-vs-redux/src/main.tsx
+++ poc-zustand-vs-redux/src/main.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { RecoilRoot } from "recoil";
 import App from "./App";
 import { store } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <RecoilRoot>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </RecoilRoot>
   </React.StrictMode>
 );
